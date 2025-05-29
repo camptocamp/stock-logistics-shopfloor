11d733ccc8d306d68a15f9c79874a9bf5255f226
Simone Orsi <simahawk@gmail.com>
2025-05-29 16:52:17 +0200

    shopfloor: v18 mig actions_stock

--- shopfloor/actions/stock.py
+++ shopfloor/actions/stock.py
@@ -119,7 +119,7 @@ class StockAction(Component):
                     _("Someone is already working on these transfers")
                 )
         for line in move_lines:
-            qty_done = quantity if quantity is not None else line.reserved_uom_qty
+            qty_done = quantity if quantity is not None else line.quantity
             line.qty_done = qty_done
             line._split_partial_quantity()
             data = {
