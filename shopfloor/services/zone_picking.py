7f047d9c5b4499353de7511536c472ff13445427
Simone Orsi <simahawk@gmail.com>
2025-08-01 12:15:50 +0200

    shopfloor: migrate zone_picking to v18

--- shopfloor/services/zone_picking.py
+++ shopfloor/services/zone_picking.py
@@ -450,7 +450,8 @@ class ZonePicking(Component):
     def _find_buffer_move_lines_domain(self, dest_package=None):
         domain = [
             ("picking_id.picking_type_id", "in", self.picking_types.ids),
-            ("qty_done", ">", 0),
+            ("qty_picked", ">", 0),
+            ("picked", "=", True),
             ("state", "not in", ("cancel", "done")),
             ("result_package_id", "!=", False),
             ("shopfloor_user_id", "=", self.env.user.id),
@@ -685,6 +686,9 @@ class ZonePicking(Component):
                     response_error_func=self._response_for_change_pack_lot,
                 )
         else:
+            # FIXME: this case shouldn't be possible anymore
+            # because a check on stock.quant.package.write in odoo core
+            # prevents to update a location on a package w/o move lines.
             response = self._list_move_lines(sublocation or self.zone_location)
             message = self.msg_store.package_has_no_product_to_take(barcode)
         return response, message
@@ -952,7 +956,7 @@ class ZonePicking(Component):
             for _move_line in package.move_line_ids:
                 if _move_line.state not in ("assigned", "partially_available"):
                     continue
-                _move_line.qty_done = move_line.quantity
+                _move_line._pick_qty(move_line.quantity)
                 move_lines |= _move_line
         self._write_destination_on_lines(move_lines, location)
 
@@ -1349,7 +1353,7 @@ class ZonePicking(Component):
             ("package_id", "=", package.id),
             ("lot_id", "=", lot.id),
             ("state", "not in", ("cancel", "done")),
-            ("qty_done", "=", 0),
+            ("qty_picked", "=", 0),
         ]
         return domain
 
