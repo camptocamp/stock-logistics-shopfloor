9145aa6ff032eb089635004bc6aa9612673a5178
Simone Orsi <simahawk@gmail.com>
2025-08-08 13:40:50 +0200

    fixup! shopfloor_dangerous_goods_mobile: migrate to v18

--- shopfloor_dangerous_goods_mobile/static/wms/src/js/demo/demo.checkout.esm.js
+++ shopfloor_dangerous_goods_mobile/static/wms/src/js/demo/demo.checkout.esm.js
@@ -97,7 +97,7 @@ const DEMO_CHECKOUT_DG = {
     scan_package_action: function (data) {
         const res = data_for_select_package;
         const line = res.data.select_package.selected_move_lines.find(function (x) {
-            return x.product.barcode == data.barcode;
+            return x.product.barcode === data.barcode;
         });
         line.qty_done++;
         return res;
@@ -127,7 +127,7 @@ const DEMO_CHECKOUT_DG = {
     set_line_qty: function (data) {
         const res = data_for_set_line_qty;
         const line = res.data.select_package.selected_move_lines.filter(function (x) {
-            return x.id == data.move_line_id;
+            return x.id === data.move_line_id;
         })[0];
         line.qty_done = line.quantity;
         return res;
@@ -135,7 +135,7 @@ const DEMO_CHECKOUT_DG = {
     reset_line_qty: function (data) {
         const res = data_for_set_line_qty;
         const line = res.data.select_package.selected_move_lines.filter(function (x) {
-            return x.id == data.move_line_id;
+            return x.id === data.move_line_id;
         })[0];
         line.qty_done = 0;
         return res;
