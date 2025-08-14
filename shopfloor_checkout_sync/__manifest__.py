4fcff3b803d1367ca03195fafc1a29b2f362139c
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2025-08-14 13:16:48 +0200

    [MIG][18.0] shopfloor_checkout_sync

--- shopfloor_checkout_sync/__manifest__.py
+++ shopfloor_checkout_sync/__manifest__.py
@@ -3,14 +3,14 @@
     "name": "Shopfloor - Checkout Sync",
     "summary": "Glue module",
     "author": "Camptocamp, Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/wms",
+    "website": "https://github.com/OCA/stock-logistics-shopfloor",
     "category": "Warehouse Management",
-    "version": "14.0.1.2.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "maintainers": ["guewen"],
     "depends": [
         "shopfloor",
-        # OCA/stock-logistics-warehouse
+        # OCA/stock-logistics-workflow
         "stock_checkout_sync",
     ],
     "auto_install": True,
