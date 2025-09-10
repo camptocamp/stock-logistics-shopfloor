84768ebd3e0c25ca4cfad03c7bd2863f07d86b3a
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2025-09-10 17:27:35 +0200

    Add shopfloor_reception_package_dimension_mobile

---
+++ shopfloor_reception_package_dimension_mobile/static/src/scenario/reception_package_dimension.esm.js
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2025 Camptocamp SA
+ * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+ */
+
+console.log("SHOPFLOOR RECEPTION PACKAGE DIMENSION MOBILE...loaded");
+
+import {process_registry} from "/shopfloor_mobile_base/static/src/services/process_registry.esm.js";
+
+const reception_scenario = process_registry.get("reception");
+const _get_states = reception_scenario.component.methods._get_states;
+// Get the original template of the reception scenario
+const template = reception_scenario.component.template;
+const pos = template.indexOf("<!-- Package Detail Slot -->");
+const new_template =
+    template.substring(0, pos) +
+    `
+    <div class="v-card__text details">
+    <v-form ref="form_dimension">
+        <v-container style="padding-top: 0; padding-bottom: 0">
+            <v-row v-if="state.data.selected_move_line[0].package_dest.package_type.height_required">
+                <v-text-field
+                    label="Package Height"
+                    type="number"
+                    :suffix="state.data.selected_move_line[0].package_dest.height_uom"
+                    placeholder="Package Height"
+                    v-model="state.data.selected_move_line[0].package_dest.height"
+                ></v-text-field>
+            </v-row>
+        </v-container>
+    </v-form>
+    </div>
+
+    ` +
+    template.substring(pos);
+
+const ReceptionPackageDimension = process_registry.extend("reception", {
+    template: new_template,
+    "methods._get_states": function () {
+        const states = _get_states.bind(this)();
+        const _get_set_destination_data =
+            states.set_destination._get_set_destination_data;
+        const overriden = function (location) {
+            const data = _get_set_destination_data(location);
+            const height = this.line_being_handled.package_dest.height;
+            data.height = Number(height);
+            return data;
+        };
+        states.set_destination._get_set_destination_data = overriden.bind(this);
+        return states;
+    },
+});
+
+process_registry.replace("reception", ReceptionPackageDimension);
