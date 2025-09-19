a6270e66dfd89fae32cfd877457bf67dda73d4bb
Mmequignon <matthieu@fwzte.xyz>
2025-09-19 12:29:56 +0200

    shopfloor_reception: fix _schema_set_destination

--- shopfloor_reception/services/reception.py
+++ shopfloor_reception/services/reception.py
@@ -1845,7 +1845,10 @@ class ShopfloorReceptionValidatorResponse(Component):
         return {
             "selected_move_line": {
                 "type": "list",
-                "schema": {"type": "dict", "schema": self.schemas.move_line()},
+                "schema": {
+                    "type": "dict",
+                    "schema": self.schemas.move_line(with_package_type=True)
+                },
             },
             "picking": {"type": "dict", "schema": self.schemas.picking()},
         }
