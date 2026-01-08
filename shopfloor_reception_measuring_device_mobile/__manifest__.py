141280df46829cdc91898b0bc9dff2ea477b0fcb
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2026-01-08 15:37:39 +0100

    Add shopfloor_reception_measuring_device_mobile

---
+++ shopfloor_reception_measuring_device_mobile/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2026 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Shopfloor Reception Measuring Device Mobile",
+    "summary": "Frontend for the measuring device on reception scenario",
+    "version": "18.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Inventory",
+    "website": "https://github.com/OCA/stock-logistics-shopfloor",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["TDu"],
+    "license": "AGPL-3",
+    "depends": [
+        "shopfloor_reception_measuring_device",
+        "shopfloor_reception_packaging_dimension_mobile",
+    ],
+    "data": ["templates/assets.xml"],
+}
