3ddc6cfad934b174a94b0446dcdfe5b7bb06c159
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2026-08-11 13:56:47 +0200

    fixup! shopfloor: add jump to menu functionality (zp, spt)

--- shopfloor/tests/test_zone_picking_jump_to_menu.py
+++ shopfloor/tests/test_zone_picking_jump_to_menu.py
@@ -1,6 +1,7 @@
 # Copyright 2026 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
+
 from odoo.addons.shopfloor.tests.test_zone_picking_base import ZonePickingCommonCase
 
 
@@ -12,15 +13,7 @@ class ZonePickingJumpToMenu(ZonePickingCommonCase):
             "shopfloor.shopfloor_menu_demo_single_pallet_transfer"
         )
 
-    def assert_response_jump_to_menu(
-        self, response, menu, state_data=None, message=None
-    ):
-        data = {
-            "next_state": "start",
-            "menu_id": menu.id,
-        }
-        if state_data:
-            data["state_data"] = state_data
+    def assert_response_jump_to_menu(self, response, data=None, message=None):
         self.assert_response(
             response,
             next_state="jump_to_menu",
@@ -29,8 +22,9 @@ class ZonePickingJumpToMenu(ZonePickingCommonCase):
         )
 
     def test_set_destination_location_jump_to(self):
-        self.service.work.menu.sudo().jump_to_menu_id = self.menu2
-        self.service.work.menu.sudo().jump_to_menu_zone_picking_unload_all = self.menu2
+        self.service.work.menu.sudo().jump_to_zone_picking_unload_all_menu_id = (
+            self.menu2
+        )
         moves_before = self.picking1.move_ids
         move_line = moves_before.move_line_ids
         response = self.service.dispatch(
@@ -42,4 +36,9 @@ class ZonePickingJumpToMenu(ZonePickingCommonCase):
                 "confirmation": None,
             },
         )
-        self.assert_response_jump_to_menu(response, self.menu2)
+        data = {
+            "menu_id": self.menu2.id,
+            "next_state": "start",
+            "states_data": '{"start": {}}',
+        }
+        self.assert_response_jump_to_menu(response, data)
